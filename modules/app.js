c1e59db89b74c456eca8bf40fbff2a3dc38a2a2d
ajm1808 <ajm180885@gmail.com>
2020-05-18 15:43:36 +0100

    ran array tests

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,10 @@
 const add = (num1, num2) => {
     return num1 + num2;
     }
+
+    let someNames=["jeff", "bill","ben"];
+    
     module.exports = {
-        add
+        add,
+        someNames
     }
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,3 +2,7 @@ const app = require('../modules/app');
 test("must return 8 when 5 and 3 are passed", ()=> {
 expect(app.add(5,3)).toBe(8);
 });
+
+test("should contain jeff", ()=>{
+    expect(app.someNames).toContain("jeff");
+});
